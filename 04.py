4998169da818dfaa169c62657d3fead08b4caf1b
Vajda Viktor <vajda.viktor.2023f@ms.gdszeged.hu>
2024-10-01 08:37:56 +0200

    elso

---
+++ 04.py
@@ -0,0 +1,29 @@
+# num = 2 + 4517
+# print(num)
+
+# x = int(input("Addj meg egy számot 1 és 10 között! "))
+
+# num = x + 4517
+# print(num)
+
+# d = "1" + "2"
+# majom = "orángután" + "cerkóf"
+# # print(majom + " " + d)
+
+# majom = "pávián" + "maki"
+# print(majom)
+
+
+
+# edény = "bögre"
+# edény = "sárga" + edény + "görbe" + edény
+# print(edény)
+
+
+
+num1 = int(input("Addj meg egy számot: "))
+num2 = int(input("Addj meg még egy számot: "))
+eredmeny = num1 + num2
+
+print(f"{num1} és {num2} összege: {eredmeny}" )
+
